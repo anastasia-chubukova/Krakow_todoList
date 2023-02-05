b01d46ae9ba896dd2ede39c9f7dfe71cd1fb4f5d
Anastasia Chubukova <nastya.a.chubukova@gmail.com>
2023-02-05 02:40:14 +0200

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Krakow_todoList';
+
+  newItem = {
+    id: "",
+    value: "",
+    checked: false
+  };
+
+  items = [
+    { id: "id1", value: "First item", checked: false },
+    { id: "id2", value: "Second item", checked: false },
+    { id: "id3", value: "Third item", checked: false },
+    { id: "id4", value: "Fourth item", checked: false },
+  ];
 }
